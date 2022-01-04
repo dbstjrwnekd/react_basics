21fb3c13c3f354fa6160e2e8cbe0a37e84256925
dbstjrwnekd <sh4869@ajou.ac.kr>
2022-01-04 20:01:59 +0900

    asynchronous setState

--- ec-platform/src/App.js
+++ ec-platform/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      meaningOfLife: 47,
+    };
+  }
+
+  handleClick() {
+    this.setState({
+      meaningOfLife: this.state.meaningOfLife + this.props.increment,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.meaningOfLife}</p>
+          <button onClick={this.handleClick} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
