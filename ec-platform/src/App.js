5701e92b867a23abf79dbbb17a883248c56d17e9
dbstjrwnekd <sh4869@ajou.ac.kr>
2022-01-05 17:41:52 +0900

    [FE] Feat: Homepage design

--- ec-platform/src/App.js
+++ ec-platform/src/App.js
@@ -1,27 +1,10 @@
 import React, { useState } from "react";
-import Lifecycles from "./lifecycle.component";
-import logo from "./logo.svg";
-import "./App.css";
+import HomePage from "./homepage.component";
 
-function App({ text }) {
-  const [showChild, setShowChild] = useState(true);
-  const [myText, setMyText] = useState(text);
-
-  function toggleClick() {
-    setShowChild(() => !showChild);
-  }
-
-  function handleClick() {
-    setMyText(myText + "_handle");
-  }
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={toggleClick}>Toggle Lifecycles</button>
-        <button onClick={handleClick}>Update Text</button>
-        {showChild ? <Lifecycles text={myText} /> : null}
-      </header>
+    <div>
+      <HomePage />
     </div>
   );
 }
