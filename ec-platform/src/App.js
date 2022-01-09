959697cc3ed38d9f75fb625663baebbab9366fcc
sh4869 <sh4869@ajou.ac.kr>
2022-01-09 19:55:04 +0900

    [FE] Feat: Header Component

--- ec-platform/src/App.js
+++ ec-platform/src/App.js
@@ -4,12 +4,17 @@ import HomePage from "./page/homepage/homepage.component";
 import ShopPage from "./page/shoppage/shoppage.component";
 import './App.css';
 
+import Header from "./components/header/header.component";
+
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/shop' element={<ShopPage />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/shop' element={<ShopPage />} />
+      </Routes>
+    </>
   );
 }
 
