eeac5a0ee67395b05719bd0c98e4b215a666b19b
sh4869 <sh4869@ajou.ac.kr>
2022-02-06 08:48:39 +0900

    [FE] Feat: cart-item & add cart-item to cart-dropdown & cart reducer

--- ec-platform/src/components/cart-icon/cart-icon.component.jsx
+++ ec-platform/src/components/cart-icon/cart-icon.component.jsx
@@ -6,17 +6,21 @@ import "./cart-icon.styles.scss";
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { connect } from 'react-redux';
 
-function CartIcon({ toggleCartHidden }) {
+function CartIcon({ toggleCartHidden, itemCount }) {
     return (
         <div className="cart-icon" onClick={toggleCartHidden}>
             <ShoppingIcon className="shopping-icon" />
-            <span className="item-count">0</span>
+            <span className="item-count">{itemCount}</span>
         </div>
     );
 }
+
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    itemCount: cartItems.length
+});
 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
