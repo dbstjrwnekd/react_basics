2f0350a3128de444f5f193dff7b84c7c91b2cf73
sh4869 <sh4869@ajou.ac.kr>
2022-02-20 16:21:19 +0900

    [FE] Feat: shop data to firebase and get data from firebase and store it in redux

--- ec-platform/src/firebase/firebase.utils.js
+++ ec-platform/src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth'
-import { getFirestore, doc, addDoc, getDoc } from "firebase/firestore";
+import { getFirestore, doc, addDoc, getDoc, collection, getDocs, writeBatch, onSnapshot } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDBnTvM2-Zi6VEM_uTxHkYeqXChxv-ieXg",
@@ -20,7 +20,9 @@ export const createUserProfileDocument = async (userAuth, additionalData=null) =
   if(!userAuth) return;
 
   const userRef = doc(database, 'user', userAuth.uid);
+
   const snapShot = await getDoc(userRef);
+
   if(!snapShot.exists()) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
@@ -39,7 +41,35 @@ export const createUserProfileDocument = async (userAuth, additionalData=null) =
   return userRef;
 };
 
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = collection(database, collectionKey);
 
+  const batch = writeBatch(database);
+
+  objectsToAdd.forEach(obj => {
+    const newDocRef = doc(collectionRef);
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+}
+
+export const getCollectionDatas = async (collectionKey) => {
+  const collections = {};
+  const collectionRef = collection(database, collectionKey);
+  const collectionSnapshot = await getDocs(collectionRef);
+  
+  collectionSnapshot.forEach(doc => {
+      const { title, items } = doc.data();
+      collections[title.toLowerCase()] = {
+        routeName: encodeURI(title.toLowerCase()),
+        id: doc.id,
+        title,
+        items
+      };
+    });
+  return collections;
+}
 
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
